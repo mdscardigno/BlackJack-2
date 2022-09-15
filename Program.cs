c61cd160d06b12ac0cb02bcf1ca2f53ada11e36f
Milagro Scardigno <mdscardigno@gmail.com>
2022-09-14 20:36:54 -0400

    correction to suits

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Blackjack
     class Card
     {
         //initialize
-        //a card has a face and a suit
+        //a card has a face, a suit and a value
         public string Face { get; set; }
         public string Suit { get; set; }
         //to assign value to each of the cards
@@ -34,26 +34,53 @@ namespace Blackjack
                     return 0;
             }
         }
+        //returns a string with the card
         public override string ToString()
         {
             return $"The {Face} of {Suit}";
         }
-
     }
     class Deck
     {
+        //What information does a deck of cards has?
+        //can deck be a list or does it have to be a class?
         //Properties: 52 cards in a deck with 4 suits and 13 ranks.
         public List<Card> Cards { get; set; }
         //what can a deck do?
-        //shuffle
-        //deal
+        //build() fills the list with 52 cards
+        //shuffle() the deck
+        //deal() that takes the first card out of the list, removes it and gives it back to the player/dealer.
+    }
+    class Hand
+    {
+        //what does a hand has? A collection of two cards with a list.
+        //Behavior
+        //The hand has to total itself. TotalValue of the hand of cards.
+        //Receive cards.
+    }
+    //not everything that has a behavior is a class
+    class Game
+    {
+
     }
 
     class Program
     {
         static void Main(string[] args)
         {
+            DisplayGreeting();
 
+
+
+
+
+
+
+            static void DisplayGreeting()
+            {
+                System.Console.WriteLine("🂡 Welcome to Blackjack. \nThe first player to come closer to 21 or at 21 wins! Those who go over 21 loose. 🃁");
+            }
         }
+
     }
 }
