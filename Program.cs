943668252aa7a345cf4159717f0574c9fe21f93a
Milagro Scardigno <mdscardigno@gmail.com>
2022-09-18 09:16:16 -0400

    pedac comments

--- Program.cs
+++ Program.cs
@@ -7,18 +7,31 @@ namespace Blackjack
     {
         static void Main(string[] args)
         {
-            //1) Create a new deck.
-            //Propertiers: 52 cards.
+            //Welcome message and description about the game.
             DisplayGreeting();
 
+            //Make a list of cards
+            //Suits is a list of "Club", "Diamond", "Heart", or "Spade"
+            //Faces is a list of 2,3,4,5,6,7,8,9,10,Jack. Queen, King or Ace.
 
-            static void DisplayGreeting()
-            {
-                Console.WriteLine("🂡 Welcome to Blackjack. \nThe first player to come closer to 21 or at 21 wins! Those who go over 21 loose. 🃁");
-                Console.WriteLine("Press any key to play.");
-                string pressToStartPlaying = Console.ReadLine();
-            }
+            //Go trough all of the suits one at a time and in order.
+            //{
+            //////Get the current suit
+            /////Go through all of the faces one at a time and in order{
+            /////////Get the current face
+            ////////With the current suit and the current face, make a new card
+            ////////Add that card to the list of cards
+            ///////}
+            //}
+
         }
 
+    }
+
+    static void DisplayGreeting()
+    {
+        Console.WriteLine("🂡 Welcome to Blackjack. \nThe first player to come closer to 21 or at 21 wins! Those who go over 21 loose. 🃁");
+        Console.WriteLine("Press any key to play.");
+        string pressToStartPlaying = Console.ReadLine();
     }
 }
