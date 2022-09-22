92ff7c4490f4ec30e31db4116de365c79e2da24c
Milagro Scardigno <mdscardigno@gmail.com>
2022-09-21 20:27:50 -0400

    Value of the cards with if statements and default check on Value()

--- Card.cs
+++ Card.cs
@@ -29,32 +29,94 @@ namespace BlackJack
         //a card has a face, a suit and a value
         public string Face { get; set; }
         public string Suit { get; set; }
+
+        //One way of assigning value to each card
         //to assign value to each of the cards
         //Cards know what the value of each card is.
+        // public int Value()
+        // {
+        //     switch (Face)
+        //     {
+        //         case "2":
+        //         case "3":
+        //         case "4":
+        //         case "5":
+        //         case "6":
+        //         case "7":
+        //         case "8":
+        //         case "9":
+        //         case "10":
+        //             return int.Parse(Face);//it returns an integer
+        //         case "Jack":
+        //         case "Queen":
+        //         case "King":
+        //             return 10;
+        //         case "Ace":
+        //             return 11;
+        //         default:
+        //             return 0;
+        //     }
+        // }
+        //Another way of assigning value to each card
         public int Value()
         {
-            switch (Face)
+            if (Face == "2")
             {
-                case "2":
-                case "3":
-                case "4":
-                case "5":
-                case "6":
-                case "7":
-                case "8":
-                case "9":
-                case "10":
-                    return int.Parse(Face);//it returns an integer
-                case "Jack":
-                case "Queen":
-                case "King":
-                    return 10;
-                case "Ace":
-                    return 11;
-                default:
-                    return 0;
+                return 2;
             }
+            if (Face == "3")
+            {
+                return 3;
+            }
+            if (Face == "4")
+            {
+                return 4;
+            }
+            if (Face == "5")
+            {
+                return 5;
+            }
+            if (Face == "6")
+            {
+                return 6;
+            }
+            if (Face == "7")
+            {
+                return 7;
+            }
+            if (Face == "8")
+            {
+                return 8;
+            }
+            if (Face == "9")
+            {
+                return 9;
+            }
+            if (Face == "10")
+            {
+                return 10;
+            }
+            if (Face == "Jack")
+            {
+                return 10;
+            }
+            if (Face == "Queen")
+            {
+                return 10;
+            }
+            if (Face == "King")
+            {
+                return 10;
+            }
+            if (Face == "Ace")
+            {
+                return 11;
+            }
+            //we need a default check
+            return 0;
+
         }
+
         //returns a string with the card
         //teaching the card how to turn itself into a string so I can console.write it to the console
         public override string ToString()
